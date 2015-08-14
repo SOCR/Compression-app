393ef10c8c9f43628328493b0fe33f5fe11a3524
Saib Husain <s.saib.husain@gmail.com>
2015-08-14 11:50:31 -0400

    Cubic 91%

--- Compressive script.js
+++ Compressive script.js
@@ -23,7 +23,7 @@ var originx2 = secondx;
 var originy2 = secondy;
 var originx3 = thirdx;
 var originy3 = thirdy;
-var graphType=$("input[name=type]:checked").val();
+var graphType = $("input[name=type]:checked").val();
 
 //updates coefficients
 function updateXs() {
@@ -134,276 +134,338 @@ function updateBars() {
 //function makeLine() {}
 
 $(document).ready(function () {
-            console.log(thirdx, thirdy);
-            redoXs();
+    console.log(thirdx, thirdy);
+    redoXs();
 
-            updateLineData();
-
-            var vis = d3.select('#visual'),
-                WIDTH = 1000,
-                HEIGHT = 350,
-                MARGINS = {
-                    top: 20,
-                    right: 20,
-                    bottom: 20,
-                    left: 50
-                }
-            xRange = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(lineData, function (d) {
-                return d.x;
-            }), d3.max(lineData, function (d) {
-                return d.x;
-            })])
-            yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(lineData, function (d) {
-                return d.y - 1;
-            }), d3.max(lineData, function (d) {
-                return d.y;
-            })])
-
-            //setup x
-            var xAxis = d3.svg.axis()
-                .scale(xRange)
-                .tickSize(5)
-                .tickSubdivide(true),
-
-                //setup y
-                yAxis = d3.svg.axis()
-                .scale(yRange)
-                .tickSize(5)
-                .orient("left")
-                .tickSubdivide(true);
+    updateLineData();
 
 
-            vis.append("svg:g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
-                .call(xAxis);
+    if (graphType == 1) {
+        $("#myRange2").hide();
+    } else {
 
-            vis.append("svg:g")
-                .attr("class", "y axis")
-                .attr("transform", "translate(" + (MARGINS.left) + ",0)")
-                .call(yAxis);
+        $("#myRange2").show();
+    }
 
-            var lineFunc = d3.svg.line()
-                .x(function (d) {
-                    return xRange(d.x);
-                })
-                .y(function (d) {
-                    return yRange(d.y);
-                });
 
-            vis.append("svg:path")
-                .attr("class", "myLine")
-                .attr("d", lineFunc(lineData))
-                .attr("stroke", "blue")
-                .attr("stroke-width", 2)
-                .attr("fill", "none");
+    var vis = d3.select('#visual'),
+        WIDTH = 1000,
+        HEIGHT = 350,
+        MARGINS = {
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 50
+        }
+    xRange = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(lineData, function (d) {
+        return d.x;
+    }), d3.max(lineData, function (d) {
+        return d.x;
+    })])
+    yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(lineData, function (d) {
+        return d.y - 1;
+    }), d3.max(lineData, function (d) {
+        return d.y;
+    })])
 
-            makeDots(firstx, secondx, thirdx);
+    //setup x
+    var xAxis = d3.svg.axis()
+        .scale(xRange)
+        .tickSize(5)
+        .tickSubdivide(true),
 
-            //behavior for a dragged point
-            var drag = d3.behavior.drag()
-                .origin(function (d) {
-                    return d;
-                })
-                .on("drag", dragmove);
+        //setup y
+        yAxis = d3.svg.axis()
+        .scale(yRange)
+        .tickSize(5)
+        .orient("left")
+        .tickSubdivide(true);
 
-            function dragmove(d) {
-                d3.select(this).attr("transform", "translate(" + (d.x = d3.event.x) + "," + (d.y = d3.event.y) + ")");
 
-                //events to update line to fit dots
-                updateXs();
-                updateLineData();
+    vis.append("svg:g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+        .call(xAxis);
 
-                //update line
-                d3.select(".myLine").transition()
-                    .attr("d", lineFunc(lineData));
+    vis.append("svg:g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+        .call(yAxis);
 
-            }
+    var lineFunc = d3.svg.line()
+        .x(function (d) {
+            return xRange(d.x);
+        })
+        .y(function (d) {
+            return yRange(d.y);
+        });
 
-            //puts in dots
-            vis.selectAll(".nodes")
-                .data(nodes)
-                .enter().append("circle")
-                .attr("class", "nodes")
-                .attr("cx", function (d) {
-                    return xRange(d.x);
-                })
-                .attr("cy", function (d) {
-                    return yRange(d.y);
-                })
-                .attr("r", "10px")
-                .attr("fill", "black")
-                .attr("transform", function (p) {
-                    return "translate(" + p.x + "," + p.y + ")";
-                })
-                .call(drag);
+    vis.append("svg:path")
+        .attr("class", "myLine")
+        .attr("d", lineFunc(lineData))
+        .attr("stroke", "blue")
+        .attr("stroke-width", 2)
+        .attr("fill", "none");
 
-            //Bar Graph
+    makeDots(firstx, secondx, thirdx);
 
-            var canvas = d3.select("#canvas");
-            canvas.width = 500;
-            canvas.height = 350;
-            var values = [Math.abs(xthree), Math.abs(xtwo), Math.abs(xone), Math.abs(xzero)];
+    //behavior for a dragged point
+    var drag = d3.behavior.drag()
+        .origin(function (d) {
+            return d;
+        })
+        .on("drag", dragmove);
 
-            var colours = ['#A0F', '#FA0', '#0AF', '#AF0'];
+    function dragmove(d) {
+        d3.select(this).attr("transform", "translate(" + (d.x = d3.event.x) + "," + (d.y = d3.event.y) + ")");
 
-            var yOffset = 0;
+        //events to update line to fit dots
+        updateXs();
+        updateLineData();
 
-            //create scale
-            yRange2 = d3.scale.linear().range([canvas.height - MARGINS.top,
+        //update line
+        d3.select(".myLine").transition()
+            .attr("d", lineFunc(lineData));
+
+    }
+
+    //puts in dots
+    vis.selectAll(".nodes")
+        .data(nodes)
+        .enter().append("circle")
+        .attr("class", "nodes")
+        .attr("cx", function (d) {
+            return xRange(d.x);
+        })
+        .attr("cy", function (d) {
+            return yRange(d.y);
+        })
+        .attr("r", "10px")
+        .attr("fill", "black")
+        .attr("transform", function (p) {
+            return "translate(" + p.x + "," + p.y + ")";
+        })
+        .call(drag);
+
+    //Bar Graph
+
+    var canvas = d3.select("#canvas");
+    canvas.width = 500;
+    canvas.height = 350;
+    var values = [Math.abs(xthree), Math.abs(xtwo), Math.abs(xone), Math.abs(xzero)];
+
+    var colours = ['#A0F', '#FA0', '#0AF', '#AF0'];
+
+    var yOffset = 0;
+
+    //create scale
+    yRange2 = d3.scale.linear().range([canvas.height - MARGINS.top,
 MARGINS.bottom]).domain([0, 10]);
 
-            //Process the data
+    //Process the data
 
-            for (var i = 0; i < values.length; i++) {
+    for (var i = 0; i < values.length; i++) {
 
-                var datum = {
+        var datum = {
 
-                    value: yRange2(values[i]),
-                    colour: colours[i],
-                    x: 0,
-                    y: yOffset
+            value: yRange2(values[i]),
+            colour: colours[i],
+            x: 0,
+            y: yOffset
 
+        }
+
+        yOffset += (canvas.height - MARGINS.top - datum.value);
+
+        data.push(datum)
+    }
+
+    //setup y
+    yAxis2 = d3.svg.axis()
+        .scale(yRange2)
+        .tickSize(5)
+        .orient("left")
+        .tickSubdivide(true);
+
+    canvas.append("svg:g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+        .call(yAxis2);
+
+    var bars = canvas.selectAll('rect').data(data)
+
+    var cumValues = 0;
+    bars
+        .enter()
+        .append('rect')
+        .attr("class", "myBars" + function (d) {
+            return d.colour;
+        })
+        .attr({
+            width: 30,
+            x: 60,
+            y: function (d) {
+                return d.value - d.y;
+            },
+            height: function (d) {
+                return canvas.height - MARGINS.top - d.value;
+            }
+        })
+        .style({
+            fill: function (d) {
+                return d.colour
+            }
+        });
+
+    //update when buttons change
+    $("input[type]:radio").change(function () {
+        graphType = $("input[name=type]:checked").val()
+
+
+        if (graphType == 1) {
+            $("#myRange2").hide();
+            //    nodes[2].style("opacity", "1");
+        } else {
+            //   nodes[2].style("opacity", "0");
+            $("#myRange2").show();
+        }
+
+        updateXs();
+        updateLineData();
+        displayVals();
+
+        //update line
+        d3.select(".myLine").transition()
+            .attr("d", lineFunc(lineData));
+
+        //update bars
+        cumValues = 0;
+        yOffset = 0;
+        data = [];
+        var values = [Math.abs(xthree), Math.abs(xtwo), Math.abs(xone), Math.abs(xzero)];
+        for (var i = 0; i < values.length; i++) {
+
+            var datum = {
+
+                value: yRange2(values[i]),
+                colour: colours[i],
+                x: 0,
+                y: yOffset
+
+            }
+            yOffset += (canvas.height - MARGINS.top - datum.value);
+
+            data.push(datum)
+        }
+        console.log(xtwo, xone, xzero);
+        bars = canvas.selectAll('rect').data(data)
+            .style("fill", function (d) {
+                return d.colour;
+            })
+            .attr({
+                y: function (d) {
+                    return d.value - d.y;
+                },
+                height: function (d) {
+                    return canvas.height - MARGINS.top - d.value;
                 }
+            })
+            .transition(); //update works
 
-                yOffset += (canvas.height - MARGINS.top - datum.value);
+    });
+    //updates when slider changes
+    $("#myRange").change(function () {
+        slider = $("#myRange").val();
 
-                data.push(datum)
+        updateXs();
+        updateLineData();
+        displayVals();
+
+        //update line
+        d3.select(".myLine").transition()
+            .attr("d", lineFunc(lineData));
+
+        //update bars
+        cumValues = 0;
+        yOffset = 0;
+        data = [];
+        var values = [Math.abs(xthree), Math.abs(xtwo), Math.abs(xone), Math.abs(xzero)];
+        for (var i = 0; i < values.length; i++) {
+
+            var datum = {
+
+                value: yRange2(values[i]),
+                colour: colours[i],
+                x: 0,
+                y: yOffset
+
             }
+            yOffset += (canvas.height - MARGINS.top - datum.value);
 
-            //setup y
-            yAxis2 = d3.svg.axis()
-                .scale(yRange2)
-                .tickSize(5)
-                .orient("left")
-                .tickSubdivide(true);
+            data.push(datum)
+        }
+        console.log(xtwo, xone, xzero);
+        bars = canvas.selectAll('rect').data(data)
+            .style("fill", function (d) {
+                return d.colour;
+            })
+            .attr({
+                y: function (d) {
+                    return d.value - d.y;
+                },
+                height: function (d) {
+                    return canvas.height - MARGINS.top - d.value;
+                }
+            })
+            .transition(); //update works
 
-            canvas.append("svg:g")
-                .attr("class", "y axis")
-                .attr("transform", "translate(" + (MARGINS.left) + ",0)")
-                .call(yAxis2);
+    });
 
-            var bars = canvas.selectAll('rect').data(data)
+    $("#myRange2").change(function () {
+        slider2 = $("#myRange2").val();
 
-            var cumValues = 0;
-            bars
-                .enter()
-                .append('rect')
-                .attr("class", "myBars" + function (d) {
-                    return d.colour;
-                })
-                .attr({
-                    width: 30,
-                    x: 60,
-                    y: function (d) {
-                        return d.value - d.y;
-                    },
-                    height: function (d) {
-                        return canvas.height - MARGINS.top - d.value;
-                    }
-                })
-                .style({
-                    fill: function (d) {
-                        return d.colour
-                    }
-                });
-            //update when buttons change
-            $("input[type]:radio").change(function () {
-            graphType=$("input[name=type]:checked").val()
+        updateXs();
+        updateLineData();
+        displayVals();
+
+        //update line
+        d3.select(".myLine").transition()
+            .attr("d", lineFunc(lineData));
+
+        //update bars
+        cumValues = 0;
+        yOffset = 0;
+        data = [];
+        var values = [Math.abs(xthree), Math.abs(xtwo), Math.abs(xone), Math.abs(xzero)];
+        for (var i = 0; i < values.length; i++) {
+
+            var datum = {
+
+                value: yRange2(values[i]),
+                colour: colours[i],
+                x: 0,
+                y: yOffset
+
             }
-                //updates when slider changes
-                $("#myRange").change(function () {
-                    slider = $("#myRange").val();
+            yOffset += (canvas.height - MARGINS.top - datum.value);
 
-                    updateXs();
-                    updateLineData();
-                    displayVals();
+            data.push(datum)
+        }
+        console.log(xtwo, xone, xzero);
+        bars = canvas.selectAll('rect').data(data)
+            .style("fill", function (d) {
+                return d.colour;
+            })
+            .attr({
+                y: function (d) {
+                    return d.value - d.y;
+                },
+                height: function (d) {
+                    return canvas.height - MARGINS.top - d.value;
+                }
+            })
+            .transition(); //update works
 
-                    //update line
-                    d3.select(".myLine").transition()
-                        .attr("d", lineFunc(lineData));
+    });
 
-                    //update bars
-                    cumValues = 0;
-                    yOffset = 0;
-                    data = [];
-                    var values = [Math.abs(xthree), Math.abs(xtwo), Math.abs(xone), Math.abs(xzero)];
-                    for (var i = 0; i < values.length; i++) {
-
-                        var datum = {
-
-                            value: yRange2(values[i]),
-                            colour: colours[i],
-                            x: 0,
-                            y: yOffset
-
-                        }
-                        yOffset += (canvas.height - MARGINS.top - datum.value);
-
-                        data.push(datum)
-                    }
-                    console.log(xtwo, xone, xzero);
-                    bars = canvas.selectAll('rect').data(data)
-                        .style("fill", function (d) {
-                            return d.colour;
-                        })
-                        .attr({
-                            y: function (d) {
-                                return d.value - d.y;
-                            },
-                            height: function (d) {
-                                return canvas.height - MARGINS.top - d.value;
-                            }
-                        })
-                        .transition(); //update works
-
-                });
-
-                $("#myRange2").change(function () {
-                    slider2 = $("#myRange2").val();
-
-                    updateXs();
-                    updateLineData();
-                    displayVals();
-
-                    //update line
-                    d3.select(".myLine").transition()
-                        .attr("d", lineFunc(lineData));
-
-                    //update bars
-                    cumValues = 0;
-                    yOffset = 0;
-                    data = [];
-                    var values = [Math.abs(xthree), Math.abs(xtwo), Math.abs(xone), Math.abs(xzero)];
-                    for (var i = 0; i < values.length; i++) {
-
-                        var datum = {
-
-                            value: yRange2(values[i]),
-                            colour: colours[i],
-                            x: 0,
-                            y: yOffset
-
-                        }
-                        yOffset += (canvas.height - MARGINS.top - datum.value);
-
-                        data.push(datum)
-                    }
-                    console.log(xtwo, xone, xzero);
-                    bars = canvas.selectAll('rect').data(data)
-                        .style("fill", function (d) {
-                            return d.colour;
-                        })
-                        .attr({
-                            y: function (d) {
-                                return d.value - d.y;
-                            },
-                            height: function (d) {
-                                return canvas.height - MARGINS.top - d.value;
-                            }
-                        })
-                        .transition(); //update works
-
-                });
-
-            });
+});
